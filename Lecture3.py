3ae1ab909f0ef3429a23fe786f56812c482e9c69
Prabhjot2407 <prabhjot8577.beaift25@chitkara.edu.in>
2025-08-25 17:57:27 +0530

    Add files via upload

---
+++ Lecture3.py
@@ -0,0 +1,17 @@
+#LOOPS IN PYTHON
+#For loop
+ 
+for i in range(1, 1001):
+    print(i)
+
+
+fruits = ["cherry", "mango", "grapes"]
+for i in fruits:
+    print("i like", i)
+
+#While loop
+i = 1
+while i <= 10 :
+    print(i)
+    i += 1
+
