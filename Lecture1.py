e6dcf3f26a845b1ea0f8d366594920e6efd531ff
Prabhjot2407 <prabhjot8577.beaift25@chitkara.edu.in>
2025-08-25 17:56:08 +0530

    Add files via upload

---
+++ Lecture1.py
@@ -0,0 +1,47 @@
+# Sum of data types (int, float, long, char)
+
+a = 10
+b = 20
+c = a + b
+print(type(c))
+print(c)
+
+a = 11.23
+b = 5.67
+c = a + b
+print(type(c))
+print(c)
+
+a = 925145624
+b = 548965237
+c = a + b
+print(c)
+
+a = "y"
+b = "o"
+c = a + b
+print(type(c))
+print(c)
+
+#Sum of data types using inputs
+a = int(input("enter first number :"))
+b = int(input("enter second number :"))
+c = a + b
+print(c)
+
+a = float(input("enter first number :"))
+b = float(input("enter second number :"))
+c = a + b
+print(c)
+
+#Variables
+name = "prabhjot"
+age = 18
+print("my name is :", name)
+print("my age is :", age)
+
+#Variables in input 
+name = input("name :")
+age = input("age :")
+print("welcome", name)
+print("age :", age)
