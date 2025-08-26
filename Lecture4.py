aff0030eb88fdc3ef00313f68aeadac38355b1fa
Prabhjot2407 <prabhjot8577.beaift25@chitkara.edu.in>
2025-08-26 10:45:48 +0530

    Add files via upload

---
+++ Lecture4.py
@@ -0,0 +1,59 @@
+#Arithematic operators
+#a = int(input("enter first number :"))
+#b = int(input("enter second number :"))
+# c = a + b
+# print(c)
+
+#a = int(input("enter first number :"))
+#b = int(input("enter second number :"))
+# c = a - b
+# print(c)
+
+#a = int(input("enter first number :"))
+#b = int(input("enter second number :"))
+# c = a * b
+# print(c)
+
+#a = int(input("enter first number :"))
+#b = int(input("enter second number :"))
+# c = a / b
+# print(c)
+
+#a = int(input("enter first number :"))
+#b = int(input("enter second number :"))
+# c = a // b
+# print(c)
+
+# a = int(input("enter first number :"))
+# b = int(input("enter second number :"))
+# c = a % b
+# print(C)
+
+#Assignment Operators
+# a = 100
+# print(a)
+
+# a += 10
+# print(a)
+
+#a -= 10
+#print(a)
+
+# a *= 10
+# print(a)
+
+# a /= 10
+# print(a)
+
+#Conditional and Logical operators
+# a = int(input("enter first number :"))
+# b = int(input("enter second number :"))
+#print(a>b or a == b)
+
+# a = int(input("enter first number :"))
+# b = int(input("enter second number :"))
+#print(a>b and a == b)
+
+# a = int(input("enter first number :"))
+# b = int(input("enter second number :"))
+# print(not(a>b))
