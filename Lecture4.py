1f0d9ca95e54af52d37f57ad0dd220f9ee7a6597
Prabhjot2407 <prabhjot8577.beaift25@chitkara.edu.in>
2025-08-26 10:54:04 +0530

    Lecture4.py

--- Lecture4.py
+++ Lecture4.py
@@ -1,59 +1,60 @@
 #Arithematic operators
-#a = int(input("enter first number :"))
-#b = int(input("enter second number :"))
-# c = a + b
-# print(c)
+a = int(input("enter first number :"))
+b = int(input("enter second number :"))
+c = a + b
+print(c)
 
-#a = int(input("enter first number :"))
-#b = int(input("enter second number :"))
-# c = a - b
-# print(c)
+a = int(input("enter first number :"))
+b = int(input("enter second number :"))
+c = a - b
+print(c)
 
-#a = int(input("enter first number :"))
-#b = int(input("enter second number :"))
-# c = a * b
-# print(c)
+a = int(input("enter first number :"))
+b = int(input("enter second number :"))
+c = a * b
+print(c)
 
-#a = int(input("enter first number :"))
-#b = int(input("enter second number :"))
-# c = a / b
-# print(c)
+a = int(input("enter first number :"))
+b = int(input("enter second number :"))
+c = a / b
+print(c)
 
-#a = int(input("enter first number :"))
-#b = int(input("enter second number :"))
-# c = a // b
-# print(c)
+a = int(input("enter first number :"))
+b = int(input("enter second number :"))
+c = a // b
+print(c)
 
-# a = int(input("enter first number :"))
-# b = int(input("enter second number :"))
-# c = a % b
-# print(C)
+a = int(input("enter first number :"))
+b = int(input("enter second number :"))
+c = a % b
+print(C)
 
 #Assignment Operators
-# a = 100
-# print(a)
+a = 100
+print(a)
 
-# a += 10
-# print(a)
+a += 10
+print(a)
 
-#a -= 10
-#print(a)
+a -= 10
+print(a)
 
-# a *= 10
-# print(a)
+a *= 10
+print(a)
 
-# a /= 10
-# print(a)
+a /= 10
+print(a)
 
 #Conditional and Logical operators
-# a = int(input("enter first number :"))
-# b = int(input("enter second number :"))
-#print(a>b or a == b)
+a = int(input("enter first number :"))
+b = int(input("enter second number :"))
+print(a>b or a == b)
 
-# a = int(input("enter first number :"))
-# b = int(input("enter second number :"))
-#print(a>b and a == b)
+a = int(input("enter first number :"))
+b = int(input("enter second number :"))
+print(a>b and a == b)
 
-# a = int(input("enter first number :"))
-# b = int(input("enter second number :"))
-# print(not(a>b))
+a = int(input("enter first number :"))
+b = int(input("enter second number :"))
+print(not(a>b))
+
