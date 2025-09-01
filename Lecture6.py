5fc300ebfe4d82a24f79d1e8b37d0594a4c3632e
Prabhjot2407 <prabhjot8577.beaift25@chitkara.edu.in>
2025-09-01 18:27:43 +0530

    Add files via upload

---
+++ Lecture6.py
@@ -0,0 +1,33 @@
+#Functions
+# A function is a block of code that perform a specific task.
+# It helps in code reusability.
+
+# def function_name(parameters):
+    #function body
+    #return value
+
+# def greet(name):
+#     return f"hello, {name}!"
+# print(greet("Prabhjot"))
+
+# nums = [10, 20, 30]
+# print(len(nums))
+# print(max(nums))
+# print(min(nums))
+# print(sum(nums))
+
+# def square(x):
+#     return x*x
+# print(square(5))  #Calling function
+
+# def add(a, b):
+#     return a + b
+# res = add(10, 20)
+# print("the sum is:", res)
+
+# a = int(input("enter first number :"))
+# b = int(input("enter second number :"))
+# def add(a, b):
+#     return a + b
+# sum = add(a, b)
+# print("your answer is :", sum)
