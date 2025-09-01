ebf9495ad7f7a72c3acab9464a0d5642205cc206
Prabhjot2407 <prabhjot8577.beaift25@chitkara.edu.in>
2025-09-01 18:26:59 +0530

    Add files via upload

---
+++ Lecture5.py
@@ -0,0 +1,76 @@
+# Nested looping
+
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         print(i*j, end = " ")       
+#     print()
+#explanation
+# 1*1
+# 1*2
+# 1*3
+# 2*1
+# 2*2
+# 2*3
+# 3*1
+# 3*2
+# 3*3
+
+# n = 5
+# for i in range(1, n+1):
+#     for j in range(i):
+#         print("*", end = " ")
+#     print()
+
+    
+# for i in range(1, 6):
+#     for j in range(i):
+#         print(i, end = " ")
+#     print()
+
+
+# n = 1
+# for i in range(0, 6):
+#     for j in range(i):
+#         n += 1
+#         print(n-1 , end = " ")
+#     print()
+
+
+
+# rows = 5
+# for i in range( rows,0, -1):
+#     for j in range(i):
+#         print("*",  end = " ")
+#     print() 
+
+
+# n = 5
+# for i in range(1, n+1):
+#     for j in range(n-i):
+#         print(" ", end = "")
+#     for k in range(i):
+#         print("*", end = "")
+#     print()
+
+# n = 5
+# for i in range(1, n+1):
+#     for j in range(n-i):
+#         print(" ", end = "")
+#     for k in range(i):
+#         print("*", end = " ")
+#     print() 
+
+# n = 5
+# for i in range(1, n+1):
+#     for j in range(n-i):
+#         print(" ", end = "")
+#     for k in range(i):
+#         print("*", end = " ")
+#     print() 
+# for i in range(n-1, -1, -1):
+#     for j in range(n-i):
+#         print(" ", end = "")
+#     for k in range(i):
+#         print("*", end = " ")
+#     print() 
+
