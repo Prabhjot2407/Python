dea946081ce05beb18fdc9aeb02b8b51cd97b5d7
Prabhjot2407 <prabhjot8577.beaift25@chitkara.edu.in>
2025-09-11 10:38:50 +0530

    Add files via upload

---
+++ test9sep.py
@@ -0,0 +1,209 @@
+# Que 1 s
+# a = int(input("enter your number :"))
+# b = float(input("enter your number :"))
+# c = str(input("enter your number :"))
+
+# print(f"Integer value is", a)
+# print(f"Float value is", b)
+# print(f"String value is", c)
+
+
+#Que 2
+# a = 10                 
+# b = 10.2              
+# c = "Hello"            
+# d = [1, 2, 3, 4, 5]        
+# e = (5, 6, 7, 8)           
+# f = {"name": "Prabh", "age": 17}  
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+
+
+#Que 3
+# a = int(input("Enter your first number :"))
+# b = int(input("enter your second number :"))
+# print("Sum is", a+b)
+# print("Diff is", a-b)
+# print("product is", a*b)
+# print("Division is", a/b)
+
+
+#Que 4
+# c = float(input("Enter temperature in Celsius: "))
+# def temp_converter(celsius):
+#     return (celsius * 9/5) + 32
+
+# f = temp_converter(c)
+# print("Temp in farenhite is", f)
+
+
+# Que 5
+# name = input("enter your name :")
+# age = input("enter your age :")
+
+# print(f"My name is {name} and i am {age} years old now.")
+
+
+#Que 6
+# a = int(input("Enter first number :"))
+# b = int(input("Enter second number :"))
+# a,b = b,a
+# print("a is", a)
+# print("b is", b)
+
+
+# Que 7
+
+
+# Que 8
+# a = int(input("enter first number :"))
+# b = int(input("enter second number :"))
+
+# c = a + b
+# d = a - b
+# e = a * b
+# f = a / b
+# g = a // b
+# h = a % b
+
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+# print(g)
+# print(h)
+
+
+# Que 9
+# a = int(input("Enter fiest number :"))
+# b = int(input("Enter second number :"))
+# if(a == b):
+#     print("Numbers are equal")
+# else:
+#     print("Numbers are not equal")
+
+
+# Que 10
+a = 10
+b = 4
+
+print()
+
+
+# Que 11
+# n = int(input("Enter your number :"))
+# if(n >= 10 and n <= 50):
+#     print("Your number is between 10 and 50")
+# else:
+#     print("Try again")
+
+
+# Que 12
+
+
+# Que 13
+# a = 10
+# b = 10
+# c = 20
+
+# print ("a is b :", a is b)
+# print("a is c :", a is c )
+
+# print("a is not b :", a is not b)
+# print("a is not c :", a is not c)
+
+
+#Que 14
+# n = int(input("Enter your number :"))
+# if(n > 0):
+#     print("Number is positive")
+# elif(n < 0):
+#     print("Number is negative")
+# else:
+#     print("Number is zero")
+
+
+# Que 15
+# n = int(input("enter your number :"))
+# if(n%2 == 0):
+#     print("Number is Even")
+# else:
+#     print("Number is Odd")
+
+
+# Que 16
+# a = int(input("Enter your first number :"))
+# b = int(input("Enter your second number :"))
+# c = int(input("Enter your third number :"))
+# if(a > b and a > c):
+#     print("a is greatest")
+# elif(b > c):
+#     print("b is greatest")
+# else:
+#     print("c is greatest") 
+
+
+
+# Que 17
+# i = 0
+# for i in range(1, 21):
+#     print(i)
+
+
+# Que 18
+# n = 5
+# i = 0
+# while i < 10:
+#     i += 1
+#     print(n*i)
+
+
+# Que 19
+
+
+# Que 20
+
+
+# Que 21
+# i = 0
+# while i < 10:
+#     i += 1    
+#     if(i == 5):
+#         continue 
+#     print(i)
+
+
+# Que 22
+# i = 0
+# while i < 10:
+#     i += 1    
+#     if(i == 7):
+#         break 
+#     print(i)
+
+
+# Que 23
+# for i in range(1, 6):  
+#     if (i == 3):
+#         pass
+#     else:
+#         print("Hello")
+
+
+# Que 24
+
+
+
+
+
+
+
+# Que 25
+# i = 0
+# for i in range(1,11):
+#     print("Square of number",i,"is",i**2) 
